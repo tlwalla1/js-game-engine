71169ce1639fc4c22574009821a6b2b692a3c826
Tyler Wallace <tylerw710@gmail.com>
2017-01-09 18:04:50 -0700

    Update file structure and fix webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const path = require('path');
+
 module.exports = { // eslint-disable-line
   devtool: 'source-map',
   entry: './index',
   module: {
     loaders: [
+      // All files with a '.ts' extension will be
+      // handled by 'awesome-typescript-loader'.
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015'],
-        },
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
       },
+    ],
+
+    preLoaders: [
+        // All output '.js' files will have any
+        // sourcemaps re-processed by 'source-map-loader'.
+        {
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
     ],
   },
   output: {
-    path: __dirname + '/dist', // eslint-disable-line
+    path: path.resolve(__dirname, 'dist'), // eslint-disable-line
+    publicPath: '/bundle/',
     filename: '[name].bundle.js',
   },
   resolve: {
-    extensions: ['.webpack.js', '.web.js', '.js'],
+    extensions: ['', '.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new CheckerPlugin(),
+  ],
 };
